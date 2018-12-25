1fbfe554a6e4367d0c36c097f0e6632c02c3ca58
Kang.Yuchan <ikimaku0517@gmail.com>
2018-12-26 05:37:53 +0900

    Getting weather(geo,api)

---
+++ weather.js
@@ -0,0 +1,37 @@
+const API_KEY = "c722e116e8ca363e62347f08ddc79d3c";
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude
+  };
+  saveCoords(coordsObj);
+}
+function handleGeoError() {
+  console.log("Cant access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCords = localStorage.getItem(COORDS);
+  if (loadedCords === null) {
+    askForCoords();
+  } else {
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
